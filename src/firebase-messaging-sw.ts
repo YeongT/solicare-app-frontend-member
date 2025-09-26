22443a916acc74cfced999582acee0f69dc2a19a
eunjin <eunjin7254@naver.com>
2025-09-26 09:33:45 +0900

    Fix: not using .env in firebase-messaging-sw.ts

--- src/firebase-messaging-sw.ts
+++ src/firebase-messaging-sw.ts
@@ -11,12 +11,12 @@ declare const self: ServiceWorkerGlobalScope;
 
 // Firebase 설정 정보
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAdK6LJq_c2DYFcGKXJELTX1ajM0eSG2Q4",
+  authDomain: "solicare-app.firebaseapp.com",
+  projectId: "solicare-app",
+  storageBucket: "solicare-app.firebasestorage.app",
+  messagingSenderId: "700598160035",
+  appId: "1:700598160035:web:e2002781857e9a6f934d16",
 };
 
 const app = initializeApp(firebaseConfig);
